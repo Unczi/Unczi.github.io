14dce2bdfd0edd899b0546d2beabcdadddf6019b
Unczi <unczi.channel@gmail.com>
2021-05-05 10:33:58 +0200

    Texture packek listája hozzáadva.

---
+++ css/js/stlyeTexturePackek.js
@@ -0,0 +1,13 @@
+darkMode()
+
+function normalMode() {
+    document.getElementById("css").href="../css/style.css"
+    document.getElementById("normalMode").style.visibility="hidden"
+    document.getElementById("darkMode").style.visibility="visible"
+}
+
+function darkMode() {
+    document.getElementById("css").href="../css/dark.css"
+    document.getElementById("normalMode").style.visibility="visible"
+    document.getElementById("darkMode").style.visibility="hidden"
+}
